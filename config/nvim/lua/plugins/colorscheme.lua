aeb8357026b23b8efaf840ae66fcdbc776c626ee
onegitsky <ieuanw554@gmail.com>
2025-04-20 20:06:19 +0100

    Update colorscheme.lua

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,41 +1,68 @@
-return { 
-	"AlexvZyl/nordic.nvim", 
+return {
+	"AlexvZyl/nordic.nvim",
 	priority = 1000,
 	config = function()
-require("nordic").setup({
-    on_palette = function(palette) end,
-    after_palette = function(palette) end,
-    on_highlight = function(highlights, palette) end,
-    bold_keywords = false,
-    italic_comments = true,
-    transparent = {
-        bg = true,
-        float = true,
-    },
-    bright_border = false,
-    reduced_blue = true,
-    swap_backgrounds = false,
-    cursorline = {
-        bold = false,
-        bold_number = true,
-        theme = 'light',
-        blend = 0.6,
-    },
-    noice = {
-        -- Available styles: `classic`, `flat`.
-        style = 'classic',
-    },
-    telescope = {
-        -- Available styles: `classic`, `flat`.
-        style = 'classic',
-    },
-    leap = {
-        dim_backdrop = true,
-    },
-    ts_context = {
-        dark_background = false,
-    }
-})
-vim.cmd("colorscheme nordic")
-	end
+		require("nordic").setup({
+			on_palette = function(palette) end,
+			after_palette = function(palette) end,
+			on_highlight = function(highlights, palette)
+				highlights.TelescopeNormal = {
+					bg = "none",
+					fg = palette.white0,
+				}
+				highlights.TelescopeBorder = {
+					bg = "none",
+					fg = palette.gray3,
+				}
+				highlights.TelescopePromptNormal = {
+					bg = "none",
+					fg = palette.white0,
+				}
+				highlights.TelescopePromptBorder = {
+					bg = "none",
+					fg = palette.gray3,
+				}
+              			highlights.TelescopePromptPrefix = {
+					bg = "none",
+					fg = palette.orange.base,
+				}
+				highlights.TelescopeResultsNormal = {
+					bg = "none",
+					fg = palette.white1,
+				}
+				highlights.TelescopePreviewNormal = {
+					bg = "none",
+					fg = palette.white1,
+				}
+			end,
+			bold_keywords = false,
+			italic_comments = true,
+			transparent = {
+				bg = true,
+				float = true,
+			},
+			bright_border = false,
+			reduced_blue = true,
+			swap_backgrounds = false,
+			cursorline = {
+				bold = false,
+				bold_number = true,
+				theme = "light",
+				blend = 0.6,
+			},
+			noice = {
+				style = "classic",
+			},
+			telescope = {
+				style = "flat",
+			},
+			leap = {
+				dim_backdrop = true,
+			},
+			ts_context = {
+				dark_background = false,
+			},
+		})
+		vim.cmd("colorscheme nordic")
+	end,
 }
